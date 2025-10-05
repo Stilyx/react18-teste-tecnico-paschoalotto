e1460f605e0405a20a0bcfbeebbd465b588233a1
daniel <dani.perfil.profissional@gmail.com>
2025-10-05 03:36:44 -0300

    style: marvel page background

--- src/app/marvel/page.tsx
+++ src/app/marvel/page.tsx
@@ -3,7 +3,7 @@ import FeatureMarvelHeader from '@/features/marvelHeader';
 
 export default function MarvelCharacters() {
 	return (
-		<div className="dark:bg-dark-gradient w-full min-h-screen h-full">
+		<div className="dark:bg-dark-gradient bg-marvel-light-background w-full min-h-screen h-full">
 			<FeatureMarvelHeader />
 			<FeatureCharList />
 		</div>
